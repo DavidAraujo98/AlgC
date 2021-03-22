65362d2d423d6b1fe7b11e8da984bdb4585dadb2
David José Araújo Ferreira <david2araujo5@gmail.com>
2021-03-22 18:34:11 +0000

    !

--- G01/ex6.c
+++ G01/ex6.c
@@ -28,16 +28,19 @@
 #include <stdio.h>
 
 void printArray(char a[], int b[], int size);
+void printArray2(char a[], int b);
 void cumSum(int a[], int b[], int size);
 
 int main(int argc, char *argv[])
 {
-    int a[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-    printArray("a", a, 12);
+    int a[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    printArray("a", a, sizeof(a) / sizeof(*a));
 
     int b[12];
     cumSum(a, b, 12);
     printArray("b", b, 12);
+
+    printf("\n");
 }
 
 void printArray(char a[], int b[], int size)
@@ -45,7 +48,7 @@ void printArray(char a[], int b[], int size)
     printf("%s: ", a);
     for (int i = 0; i < size; i++)
     {
-        printf("%d\t", b[i]);
+        printf("%d    ", b[i]);
     }
     printf("\n");
 }
