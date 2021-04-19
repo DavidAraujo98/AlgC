87ac0cf174a75883eda3e9cf53576e1aa6e3494b
Test <test@localhost.localdomain>
2021-04-19 15:38:42 +0100

    Guiao 05

---
+++ G05/ex2.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include <stdlib.h>
+
+int a[] = 
+
+int * random(int range, size){
+		
+}
+
+int main(void){
+
+	
+
+}
+
+# https://www.tutorialspoint.com/cprogramming/c_return_arrays_from_function.htm
+
