a0838d793c44f2981772a6b909d7134100fc8b3c
Test <test@localhost.localdomain>
2021-04-20 19:13:14 +0100

    Update

--- G05/ex2.c
+++ G05/ex2.c
@@ -1,17 +1,61 @@
 #include<stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
-int a[] = 
+int comp = 0;
 
-int * random(int range, size){
-		
+int * getRandom(int range){
+	static int r[10];
+	
+	srand(time(0));
+	
+	for(int i = 0; i < 10; ++i){
+		r[i] = rand() % range + 1;
+	}
+	
+	return r;
+}
+
+int seq(int *array){
+	int elem = 1;
+	for(int i = 1; i < 10; i++){
+		int dif = 1;
+		for (int j = 0; j < i; j++){
+			comp += 2;
+			if( *(array+i) % *(array+j) == 0 || *(array+j) % *(array+i) == 0) {
+				dif = 0	;
+				break;
+			}
+		}
+		if(dif == 1){
+			*(array+elem) = *(array+i);
+			elem++;
+		}			
+	}	
+	return elem;
 }
 
 int main(void){
+	int range = 10;
 
+	printf("Range (from 0): ");
+	scanf("%d", &range);
+
+	int *a = getRandom(range);
 	
+	printf("Array is: [");
+	for(int i = 0; i < 10; i++){
+		printf("%d,", *(a+i));
+	}
+	printf("]\n");
+	
+	int nsize = seq(a);
+	
+	printf("New array is: [");
+	for(int i = 0; i < nsize; i++){
+		printf("%d,", *(a+i));
+	}
+	printf("]\n");
 
+	printf("Comparações: %d\n", comp);
 }
-
-# https://www.tutorialspoint.com/cprogramming/c_return_arrays_from_function.htm
-
