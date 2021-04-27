0860cbe071f51ab350986b9858e80b25842ba033
David José Araújo Ferreira <david2araujo5@gmail.com>
2021-04-28 00:19:23 +0100

    Guiao 06

--- G05/ex2.c
+++ G05/ex2.c
@@ -16,28 +16,32 @@ int * getRandom(int range){
 	return r;
 }
 
-void seq(int *array, int *size){
+void seq(int *array, int *copy, int *size){
 	int elem = 0;
 	for(int i = 0; i < *size; i++){
 		comp++;
 		if(*(array+i) != 1){
 			int dif = 1;
-			for (int j = 0; j < i; j++){
+			for (int j = i-1; j >= 0; j--){
 				comp++;
 				if( *(array+i) % *(array+j) == 0) {
 					dif = 0	;
 					break;
-				}else{
-					comp++;
-					if( *(array+j) % *(array+i) == 0) {
-						dif = 0	;
-						break;
-					}
-				} 
+				}
+				comp++;
+				if( *(array+j) % *(array+i) == 0) {
+					dif = 0	;
+					break;
+				}
+				comp++;
+				if( *(array+j) == *(array+i)) {
+					dif = 0	;
+					break;
+				}
 			}
-			comp++
+			comp++;
 			if(dif == 1){
-				*(array+elem) = *(array+i);
+				*(copy+elem) = *(array+i);
 				elem++;
 			}	
 		}else{
@@ -51,6 +55,7 @@ void seq(int *array, int *size){
 int main(void){
 	int range = 10;
 	int size = 10;
+	static int b[10];
 
 	printf("Range (from 0): ");
 	scanf("%d", &range);
@@ -63,7 +68,7 @@ int main(void){
 	}
 	printf("]\n");
 	
-	seq(a, &size);
+	seq(a, b, &size);
 	
 	printf("New array is: [");
 	for(int i = 0; i < size; i++){
@@ -73,4 +78,5 @@ int main(void){
 
 	printf("Comparações: %d\n", comp);
 	printf("Nº Elementos: %d\n", size);
+	
 }
