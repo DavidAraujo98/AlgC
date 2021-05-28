8a86123f331e57a9277b4c182eeaae3ebbadb451
David Araujo <david2araujo5@gmail.com>
2021-05-28 22:23:28 +0100

    Guiao 10

--- G10/PersonSet.c
+++ G10/PersonSet.c
@@ -1,5 +1,5 @@
-// NMEC: ...
-// NICK: ...
+// NMEC: 93444
+// NICK: David José Araújo Ferreira
 
 // Complete the functions (marked by ...)
 // so that it passes all tests in Tests.
@@ -19,30 +19,32 @@
 
 // Definition of the structure
 struct _PersonSet_ {
-  List *persons;  // points to a SortedList of Person pointers
+	List *persons;  // points to a SortedList of Person pointers
 };
 
 // Comparison function to be used in generic SortedList.
 // Comparison is based on Person ID
 static int cmpP(const void *a, const void *b) {
-  Person *p1 = (Person *)a;
-  Person *p2 = (Person *)b;
-  int d = p1->id - p2->id;
-  return (d > 0) - (d < 0);
+	Person *p1 = (Person *)a;
+	Person *p2 = (Person *)b;
+	int d = p1->id - p2->id;
+	return (d > 0) - (d < 0);
 }
 
 // Create a PersonSet.
 PersonSet *PersonSetCreate() {
-  // You must allocate space for the struct and create an empty persons list!
-  //...
-  
+	// You must allocate space for the struct and create an empty persons list!
+	//...
+	PersonSet* p = (PersonSet*) malloc(sizeof(*p));
+	p->persons = (List*)malloc(sizeof(p->persons));
 }
 
 // Destroy PersonSet *pps
 void PersonSetDestroy(PersonSet **pps) {
-  assert(*pps != NULL);
-  //...
-  
+	assert(*pps != NULL);
+	free((*pps)->persons);
+	free(*pps);
+	*pps = NULL;  
 }
 
 int PersonSetSize(const PersonSet *ps) { return ListGetSize(ps->persons); }
@@ -50,78 +52,112 @@ int PersonSetSize(const PersonSet *ps) { return ListGetSize(ps->persons); }
 int PersonSetIsEmpty(const PersonSet *ps) { return ListIsEmpty(ps->persons); }
 
 void PersonSetPrint(const PersonSet *ps) {
-  printf("{\n");
-  for (ListMoveToHead(ps->persons); ListCurrentIsInside(ps->persons);
-       ListMoveToNext(ps->persons)) {
-    Person *p = (Person *)ListGetCurrentItem(ps->persons);
-    PersonPrintf(p, ";\n");
-  }
-  printf("}(size=%d)\n", PersonSetSize(ps));
-  ListTestInvariants(ps->persons);
+	printf("{\n");
+	for (ListMoveToHead(ps->persons); ListCurrentIsInside(ps->persons); ListMoveToNext(ps->persons)) {
+		Person *p = (Person *)ListGetCurrentItem(ps->persons);
+		PersonPrintf(p, ";\n");
+	}
+	printf("}(size=%d)\n", PersonSetSize(ps));
+	ListTestInvariants(ps->persons);
 }
 
 // Find node in list ps->persons of person with given id.
 // (INTERNAL function.)
 static int search(const PersonSet *ps, int id) {
-  Person dummyperson;
-  dummyperson.id = id;
-  return ListSearch(ps->persons, &dummyperson);
+	Person dummyperson;
+	dummyperson.id = id;
+	return ListSearch(ps->persons, &dummyperson);
 }
 
 // Add person *p to *ps.
 // Do nothing if *ps already contains a person with the same id.
 void PersonSetAdd(PersonSet *ps, Person *p) {
-  //...
-  
+	ListInsert(ps->persons,p);
 }
 
 // Pop one person out of *ps.
 Person *PersonSetPop(PersonSet *ps) {
-  assert(!PersonSetIsEmpty(ps));
-  // It is easiest to pop and return the person in the first position!
-  //...
-  
+	assert(!PersonSetIsEmpty(ps));
+	// It is easiest to pop and return the person in the first position!
+	//...
+	ListMoveToHead(ps->persons);
+	Person* p = ListGetCurrentItem(ps->persons);
+	ListRemoveHead(ps->persons);
+	return p;
 }
 
 // Remove the person with given id from *ps, and return it.
 // If no such person is found, return NULL and leave set untouched.
 Person *PersonSetRemove(PersonSet *ps, int id) {
-  // You may call search here!
-  //...
-  
+	// You may call search here!
+	//...
+	if(ListSearch(ps->persons, id)){
+			ListRemoveCurrent(ps->persons);
+	}
+
 }
 
 // Get the person with given id of *ps.
 // return NULL if it is not in the set.
 Person *PersonSetGet(const PersonSet *ps, int id) {
-  // You may call search here!
-  //...
-  
+	// You may call search here!
+	//...
+	if(ListSearch(ps->persons, id)){
+		return ListGetCurrentItem(ps->persons);
+	}
+	return NULL;
 }
 
 // Return true (!= 0) if set contains person wiht given id, false otherwise.
 int PersonSetContains(const PersonSet *ps, int id) {
-  return search(ps, id) >= 0;
+	return search(ps, id) >= 0;
 }
 
 // Return a NEW PersonSet with the union of *ps1 and *ps2.
 // NOTE: memory is allocated.  Client must call PersonSetDestroy!
 PersonSet *PersonSetUnion(const PersonSet *ps1, const PersonSet *ps2) {
-  PersonSet *ps = PersonSetCreate();
+	PersonSet *ps = PersonSetCreate();
 
-  // Merge the two sorted lists (similar to mergesort).
-  //...
+	// Merge the two sorted lists (similar to mergesort).
+	//...
+	
+	while(ListGetCurrentPos(ps1->persons)){
+		ListInsert(ps->persons, ListGetCurrentItem(ps1->persons));
+		ListMoveToNext(ps1->persons);
+	}
+	
+	while(ListGetCurrentPos(ps2->persons)){
+		ListInsert(ps->persons, ListGetCurrentItem(ps2->persons));
+		ListMoveToNext(ps2->persons);
+	}
   
-  return ps;
+	return ps;
 }
 
 // Return a NEW PersonSet with the intersection of *ps1 and *ps2.
 // NOTE: memory is allocated.  Client must call PersonSetDestroy!
 PersonSet *PersonSetIntersection(const PersonSet *ps1, const PersonSet *ps2) {
-  PersonSet *ps = PersonSetCreate();
-  //...
-  
-  return ps;
+	PersonSet *ps = PersonSetCreate();
+	//...
+	while(ListGetCurrentPos(ps1->persons)){
+		int v = 0;
+		while(ListGetCurrentPos(ps2->persons)){
+		
+			v = (ps1->persons)->compare(ListGetCurrentItem(ps1->persons), ListGetCurrentItem(ps2->persons));
+			if(!v){break;}
+			
+			ListMoveToNext(ps2->persons);
+		}
+		
+		if(!v){
+				ListInsert(ps->persons, ListGetCurrentItem(ps1->persons));
+		}
+			
+		
+		ListMoveToHead(ps2->persons);
+		ListMoveToNext(ps1->persons);
+	}
+	return ps;
 }
 
 // Return a NEW PersonSet with the set difference of *ps1 and *ps2.
